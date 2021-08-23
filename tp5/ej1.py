25501e17f810af854260692353521c3d0c3453d9
Nacho <nachorito05@gmail.com>
2021-08-23 03:10:46 -0300

    varios

--- tp5/ej1.py
+++ tp5/ej1.py
@@ -1,7 +1,46 @@
-#DEsarrolle una funcion para ingresar por teclado un nro natural , la funcion rechazara cualquier invalida de datos mostrando excepciones mostrando el error
-#controlar que se ingrese el nro que sea entero, que sea mayor que 0 y devolver cuando sea correcto, tambien mostrar correcto funcionamiento
+class FueraDeRango(Exception): #Genero mi propia excepción.
+   ... 
+
+class ValorMenorMin(Exception):
+   ... 
+
+class ValorMayorMax(Exception):
+    ...
+
+def ingresar_numero(mensaje, min, max):
+    numero=0
+    while True:
+        try:
+            texto=input(mensaje)
+            numero = convertir_texto_numero(texto, min, max)
+            return numero
+        except ValueError:
+            print('Debe ingresar un número.')
+        #except FueraDeRango:
+            #print('Ingrese un nro de 1 a 100')
+        except ValorMayorMax:
+            print('El valor ingresado es mayor a ', max)
+        except ValorMenorMin:
+            print('El valor ingresado es menor a ', min)
+
+def convertir_texto_numero(texto, min, max):
+    numero = int(texto)
+
+    if numero < min:
+        raise ValorMenorMin
+
+    if numero > max:
+        raise ValorMayorMax
+    return numero
+
 
 def main():
+
+   # edad = ingresar_numero('Ingrese su edad: ', 0, 150)
+   # print(edad)
+
+    nota = ingresar_numero('Ingrese la nota: ', 1, 10)
+    print(nota)
     ...
 
 
@@ -9,5 +48,4 @@ if __name__ == '__main__':
     main()
     ...
 
-def ingresar_numero():
-    ...
+
